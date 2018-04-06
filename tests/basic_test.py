7ac6a820728ac27e49efc58722edcd990de4fb81
Brett LaBombarda <bplabombarda@gmail.com>
2018-04-05 20:41:35 -0500

    Adding second dummy test.

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,5 +1,11 @@
-def func(x):
+def inc(x):
 	return x + 1
 
-def test_answer():
-	assert func(4) == 5
+def dec(x):
+	return x - 1
+
+def test_increment():
+	assert inc(3) == 4
+
+def test_decrement():
+	assert dec(3) == 2
